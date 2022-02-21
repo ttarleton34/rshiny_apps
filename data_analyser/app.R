991c2bef3175b8ce5f8e2781c66af60bf9e17a97
ttarleton34 <ttarleton34@gmail.com>
2022-02-21 22:25:56 +0100

    Updating to most current version of the app

--- data_analyser/app.R
+++ data_analyser/app.R
@@ -4,6 +4,11 @@ library(data.table)
 library(ggplot2)
 library(mlma)
 library(fresh)
+library(readxl)
+library(stringr)
+library(tidyverse)
+library(xlsx)
+
 
 not_sel <- "Not Selected"
 
@@ -24,37 +29,50 @@ main_page <- tabPanel(
   sidebarLayout(
     sidebarPanel(
       title = "Inputs",
-      fileInput("csv_input", "Select CSV File to Import", accept = ".csv"),
+      fileInput("csv_input", "Select CSV, TXT, or XLSX File to Import", accept = c(".csv", ".xlsx", ".txt")),
       selectInput("num_var_1", "Predictor: X", choices = c(not_sel)),
       selectInput("num_var_2", "Response: Y", choices = c(not_sel)),
       selectInput("num_var_3", "Mediators", choices = c(not_sel), multiple = TRUE),
       selectInput("fact_var", "Level of X", choices = c(not_sel)),
+      numericInput(
+        inputId = "num_bootstrap_replicates",
+        label = "Number of Bootstrap Replications (Default is 500)",
+        value = 500,
+        min = 0,
+        max = NA,
+        step = NA,
+        width = NULL
+      ),
       br(),
       actionButton("run_button", "Run Analysis", icon = icon("play"))
     ),
     mainPanel(
       tabsetPanel(
         tabPanel(
-          title = "Model Graphic Outputs",
+          title = "Graphic Outputs",
           plotOutput("plot_1")
         ),
         tabPanel(
-          title = "Model Statistics",
-          verbatimTextOutput("model_summary")
-        )
+          title = "Model Statistics I",
+          verbatimTextOutput("model_summary_1")
+        ),
+        tabPanel(
+          title = "Model Statistics II",
+          verbatimTextOutput("model_summary_2")
+        ),
       )
     )
   )
 )
 
-draw_plot_1 <- function(data_input, num_var_1, num_var_2, num_var_3, fact_var){
+draw_plot_1 <- function(data_input, num_var_1, num_var_2, num_var_3, fact_var, num_bootstrap_replicates){
   if(num_var_1 != not_sel & num_var_2 != not_sel  & num_var_3 != not_sel & fact_var != not_sel){
-    x <- data_input[,get(num_var_1)]
-    y <- data_input[,get(num_var_2)]
-    m1 <- data_input[,get(num_var_3[1])]
-    m2 <- data_input[,get(num_var_3[2])]
-    m3 <- data_input[,get(num_var_3[3])]
-    lv <- data_input[,get(fact_var)]
+    x <- data_input[,num_var_1]
+    y <- data_input[,num_var_2]
+    m1 <- data_input[,num_var_3[1]]
+    m2 <- data_input[,num_var_3[2]]
+    m3 <- data_input[,num_var_3[3]]
+    lv <- data_input[,fact_var]
     mat = matrix(ncol = 0, nrow = length(m1))
     m4=data.frame(mat)
     # for(vector in num_var_3){
@@ -66,19 +84,19 @@ draw_plot_1 <- function(data_input, num_var_1, num_var_2, num_var_3, fact_var){
                     f10y=list(1,c("x^2","sqrt(x+6)")), 
                     f20ky=list(1,c("x","x^3")), 
                     f10km=list(matrix(c(1,1),1),"log(x+2)"), level=sim.111$level)
-    temp2<-mlma(y=sim.111$y, data1=data2)
-    plot(temp2, col = "red") #graphic output
+    tmp2.boot<-boot.mlma(y=sim.111$y, data1=data2, boot=num_bootstrap_replicates,echo=F)
+    plot(tmp2.boot) #Graphic Outputs
   }
 }
 
-create_num_var_table <- function(data_input, num_var_1, num_var_2, num_var_3, fact_var){
+create_num_var_table_1 <- function(data_input, num_var_1, num_var_2, num_var_3, fact_var){
   if(num_var_1 != not_sel & num_var_2 != not_sel  & num_var_3 != not_sel & fact_var != not_sel){
-    x <- data_input[,get(num_var_1)]
-    y <- data_input[,get(num_var_2)]
-    m1 <- data_input[,get(num_var_3[1])]
-    m2 <- data_input[,get(num_var_3[2])]
-    m3 <- data_input[,get(num_var_3[3])]
-    lv <- data_input[,get(fact_var)]
+    x <- data_input[,num_var_1]
+    y <- data_input[,num_var_2]
+    m1 <- data_input[,num_var_3[1]]
+    m2 <- data_input[,num_var_3[2]]
+    m3 <- data_input[,num_var_3[3]]
+    lv <- data_input[,fact_var]
     # print(typeof(m1))
     # write(typeof(m1), stdout())
     mat = matrix(ncol = 0, nrow = length(m1))
@@ -98,14 +116,42 @@ create_num_var_table <- function(data_input, num_var_1, num_var_2, num_var_3, fa
   }
 }
 
+create_num_var_table_2 <- function(data_input, num_var_1, num_var_2, num_var_3, fact_var, num_bootstrap_replicates){
+  if(num_var_1 != not_sel & num_var_2 != not_sel  & num_var_3 != not_sel & fact_var != not_sel){
+    x <- data_input[,num_var_1]
+    y <- data_input[,num_var_2]
+    m1 <- data_input[,num_var_3[1]]
+    m2 <- data_input[,num_var_3[2]]
+    m3 <- data_input[,num_var_3[3]]
+    lv <- data_input[,fact_var]
+    # print(typeof(m1))
+    # write(typeof(m1), stdout())
+    mat = matrix(ncol = 0, nrow = length(m1))
+    m4=data.frame(mat)
+    # for(vector in num_var_3){
+    #   m=cbind(m,vector)
+    # }
+    # m=m1
+    m=cbind(m1,m2,m3,m4)
+    sim.111=list(x=x,m=m,y=y,level=lv)
+    data2<-data.org(sim.111$x, m=data.frame(sim.111$m), 
+                    f10y=list(1,c("x^2","sqrt(x+6)")), 
+                    f20ky=list(1,c("x","x^3")), 
+                    f10km=list(matrix(c(1,1),1),"log(x+2)"), level=sim.111$level)
+    tmp2.boot<-boot.mlma(y=sim.111$y, data1=data2, boot=num_bootstrap_replicates,echo=F)
+    summary(tmp2.boot) #Graphic Outputs
+  }
+}
+
 ui <- navbarPage(
   title = "LSU - Mediation Analysis Project",
   
   use_theme(create_theme(theme = "flatly", 
   bs_vars_navbar(default_bg = "#dfb52a", default_border = "#301456", default_link_color = "#301456", default_link_active_color = "#301456", 
-                 default_link_hover_bg = "#FFFFFF", ), 
+                 default_link_hover_color = "#FFFFFF", ), 
   bs_vars_global(body_bg = "#301456",text_color = "#dfb52a",link_color = "#dfb52a",link_hover_color = "#dfb52a",), 
-  bs_vars_tabs(active_link_hover_color = "#dfb52a",))),
+  bs_vars_tabs(active_link_hover_color = "#dfb52a",), bs_vars_button(
+    default_color = "#301456",default_bg = "#dfb52a",))),
   
   main_page,
   about_page
@@ -117,7 +163,18 @@ server <- function(input, output){
 
   data_input <- reactive({
     req(input$csv_input)
-    fread(input$csv_input$datapath)
+    if(str_ends(input$csv_input$datapath, "csv")) 
+    {
+      read.csv(input$csv_input$datapath)
+    }
+    else if (str_ends(input$csv_input$datapath, "(xlsx|xls)")) 
+    {
+      read.xlsx(input$csv_input$datapath, sheetIndex = 1)
+    }
+    else if(str_ends(input$csv_input$datapath, "txt"))
+    {
+      read.table(input$csv_input$datapath, header = TRUE)
+    }
   })
 
   observeEvent(data_input(),{
@@ -125,29 +182,39 @@ server <- function(input, output){
     updateSelectInput(inputId = "num_var_1", choices = choices)
     updateSelectInput(inputId = "num_var_2", choices = choices)
     updateSelectInput(inputId = "num_var_3", choices = choices)
+    updateNumericInput(inputId = "num_bootstrap_replicates")
     updateSelectInput(inputId = "fact_var", choices = choices)
   })
 
   num_var_1 <- eventReactive(input$run_button,input$num_var_1)
   num_var_2 <- eventReactive(input$run_button,input$num_var_2)
   num_var_3 <- eventReactive(input$run_button,input$num_var_3)
+  num_bootstrap_replicates <- eventReactive(input$run_button,input$num_bootstrap_replicates)
   fact_var <- eventReactive(input$run_button,input$fact_var)
 
-  # plot
+  # Plot
   
   plot_1 <- eventReactive(input$run_button,{
-    draw_plot_1(data_input(), num_var_1(), num_var_2(), num_var_3(), fact_var())
+    draw_plot_1(data_input(), num_var_1(), num_var_2(), num_var_3(), fact_var(), num_bootstrap_replicates())
   })
 
   output$plot_1 <- renderPlot(plot_1())
   
-  # 1-d summary tables
+  # Summary Table without Bootstrapping
 
   num_var_1_summary_table <- eventReactive(input$run_button,{
-    create_num_var_table(data_input(),num_var_1(),num_var_2(),num_var_3(),fact_var())
+    create_num_var_table_1(data_input(),num_var_1(),num_var_2(),num_var_3(),fact_var())
   })
 
-  output$model_summary <- renderPrint(num_var_1_summary_table())
+  output$model_summary_1 <- renderPrint(num_var_1_summary_table())
+  
+  # Summary Table Using Bootstrapping
+  
+  num_var_2_summary_table <- eventReactive(input$run_button,{
+    create_num_var_table_2(data_input(),num_var_1(),num_var_2(),num_var_3(),fact_var(),num_bootstrap_replicates())
+  })
+  
+  output$model_summary_2 <- renderPrint(num_var_2_summary_table())
 
 }
 
